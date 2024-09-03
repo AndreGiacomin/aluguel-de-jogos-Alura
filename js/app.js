25cbfe5af032c73de3d3683feb5d8a4e58ec90ed
André B Giacomini <123978805+AndreGiacomin@users.noreply.github.com>
2024-09-03 08:42:15 -0300

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,22 @@
+function alterarStatus(id){
+    let gameClicado = document.getElementById(`game-${id}`);
+    let imagem = gameClicado.querySelector('.dashboard__item__img');
+    let botao = gameClicado.querySelector('.dashboard__item__button');
+    let nomeJogo = gameClicado.querySelector('.dashboard__item__name').textContent;
+    
+    if (imagem.classList.contains('dashboard__item__img--rented')) {
+        if (confirm(`Tem certeza que deseja devolver o jogo "${nomeJogo}"?`)) {
+        imagem.classList.remove('dashboard__item__img--rented');
+        botao.classList.remove('dashboard__item__button--return');
+        botao.textContent = 'Alugar';
+
+        } else {
+            return;
+        }
+    
+    } else {
+        imagem.classList.add('dashboard__item__img--rented');
+        botao.textContent = 'Devolver';
+        botao.classList.add('dashboard__item__button--return');
+    }
+}
